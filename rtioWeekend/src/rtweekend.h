ab824b5b3d11e40ed00b4a1c1f64da677965d74c
瑞杰 许 <rjxu@mail.sdu.edu.cn>
2021-03-30 19:52:58 +0800

    Added antialiasing.

--- rtioWeekend/src/rtweekend.h
+++ rtioWeekend/src/rtweekend.h
@@ -4,7 +4,13 @@
 #include <cmath>
 #include <limits>
 #include <memory>
+#include <random>
 
+inline double random_double() {
+	static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+	static std::mt19937 generator;
+	return distribution(generator);
+}
 
 // Usings
 
@@ -21,6 +27,12 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
 	return degrees * pi / 180.0;
+}
+
+inline double clamp(double x, double min, double max) {
+	if (x < min) return min;
+	if (x > max) return max;
+	return x;
 }
 
 // Common Headers
